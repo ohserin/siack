1bf4a3ff491ae816af3ef488f3100712cb36e880
SG5143 <sun5143a@gmail.com>
2025-07-05 17:45:41 +0900

    feat: implement user domain including VO, DTO, Repository, Service, and RequestDto

--- src/main/java/com/dakgu/siack/user/domain/User.java
+++ src/main/java/com/dakgu/siack/user/domain/User.java
@@ -1,13 +1,38 @@
 package com.dakgu.siack.user.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.dakgu.siack.util.Timestamp;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name="users")
-public class User {
+@Table(name = "users")
+public class User extends Timestamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(length = 50, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 30)
+    private String phone;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+    @Column(nullable = false)
+    private int role;
 }
