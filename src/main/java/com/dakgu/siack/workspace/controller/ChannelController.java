d7f45bf6ef8b39bf2046a2aa4b64eca57c6175d8
SG5143 <sun5143a@gmail.com>
2025-09-28 21:23:57 +0900

    [기능추가][워크스페이스] ChannelController 클래스 추가 및 채널 생성 API 구현

---
+++ src/main/java/com/dakgu/siack/workspace/controller/ChannelController.java
@@ -0,0 +1,26 @@
+package com.dakgu.siack.workspace.controller;
+
+import com.dakgu.siack.utils.ResponseDTO;
+import com.dakgu.siack.workspace.dto.CreateChannelRequestDTO;
+import com.dakgu.siack.workspace.service.ChannelRequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/channel")
+@RequiredArgsConstructor
+public class ChannelController {
+
+    private final ChannelRequestService channelRequestService;
+
+    @PostMapping("/{workspaceId}/create")
+    public ResponseEntity<?> createChannel(Authentication authentication,
+                                           @PathVariable("workspaceId") Long workspaceId,
+                                           @RequestBody CreateChannelRequestDTO request) {
+        ResponseDTO response = channelRequestService.createChannel(authentication, workspaceId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
