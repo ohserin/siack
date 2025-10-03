b52030214bce62404015f43cb5e3dc3153a979b0
SG5143 <sun5143a@gmail.com>
2025-10-03 21:17:55 +0900

    [기능개선][빌드] Vite 설정에 chunkSizeWarningLimit 및 수동 청크 추가

--- src/main/frontend/vite.config.js
+++ src/main/frontend/vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 허용
-    port: 3333,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8080', // 스프링 부트 백엔드 주소
-        changeOrigin: true, // 호스트 헤더를 target으로 변경
-        secure: false // HTTPS가 아닌 경우 필요
-      }
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        port: 3333,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8080', // 스프링 부트 백엔드 주소
+                changeOrigin: true, // 호스트 헤더를 target으로 변경
+                secure: false // HTTPS가 아닌 경우 필요
+            }
+        }
+    },
+    build: {
+        chunkSizeWarningLimit: 1500,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ['react', 'react-dom'],
+                }
+            }
+        }
     }
-  }
-});
+})
