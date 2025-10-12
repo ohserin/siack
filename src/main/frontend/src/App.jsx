344eabc53da1aa1cbac95404bea7e0f2a8c4d458
SG5143 <sun5143a@gmail.com>
2025-10-12 19:50:13 +0900

    [기능개선][워크스페이스] 라우팅 구조 개선을 위한 routes.js 파일 추가 및 App.jsx 수정

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,19 +1,11 @@
 import Header from './components/Header';
-// import Footer from './components/Footer';
-import Home from './pages/Home';
-import Login from './pages/users/Login.jsx';
-import Join from "./pages/users/Join.jsx";
 import {AuthProvider} from './contexts/AuthContext.jsx';
 import {Box, Container} from '@mui/material';
 import {Routes, Route, useNavigate, useLocation} from 'react-router-dom';
 import {setNavigator} from "./utils/navigation.js";
 import {useEffect} from "react";
 import {ModalProvider} from "./contexts/ModalContext.jsx";
-import Contact from "./pages/Contact.jsx";
-import UserSettingsPage from "./pages/users/settings/UserSettingsPage.jsx";
-import DeletedAccount from './pages/users/DeletedAccount.jsx';
-import {CreateWorkspace} from './pages/workspace';
-import {Workspace} from './pages/workspace';
+import routes from './routes';
 
 function App() {
     const nav = useNavigate();
@@ -30,17 +22,11 @@ function App() {
                 {!hideHeader && <Header/>}
                 <Container maxWidth={false} disableGutters sx={{flex: 1, px: 0}}>
                     <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/join" element={<Join/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                        <Route path="/contact" element={<Contact/>}/>
-                        <Route path="/user-setting" element={<UserSettingsPage/>}/>
-                        <Route path="/account-deleted" element={<DeletedAccount/>}/>
-                        <Route path="/workspace/create" element={<CreateWorkspace/>}/>
-                        <Route path="/workspace/room/:roomId" element={<Workspace/>}/>
+                        {routes.map(({ path, element }, index) => (
+                            <Route key={index} path={path} element={element} />
+                        ))}
                     </Routes>
                 </Container>
-                {/*<Footer/>*/}
             </Box>
         </ModalProvider></AuthProvider>
     );
