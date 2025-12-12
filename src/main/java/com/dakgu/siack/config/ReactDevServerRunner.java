9436a58fab47bde134b75d5c594bdccd4f291ecb
SG5143 <sun5143a@gmail.com>
2025-12-12 21:07:52 +0900

    [기능개선][공통] 개발환경 실행 시에만 바이트 서버 구동하도록 변경

--- src/main/java/com/dakgu/siack/config/ReactDevServerRunner.java
+++ src/main/java/com/dakgu/siack/config/ReactDevServerRunner.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.DisposableBean;
 
@@ -12,6 +13,7 @@ import java.net.Socket;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+@Profile("dev")
 @Component
 public class ReactDevServerRunner implements ApplicationRunner, DisposableBean {
 
